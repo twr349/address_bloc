72f29c89893fbecc75fd1d860f300d95b828b646
twr349 <twr349@gmail.com>
2017-10-10 18:55:24 +0000

    assignment 6 completed

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -85,7 +85,30 @@ require_relative '../models/address_book'
      end
    end
     
+   describe "#imports_new_cvs_file" do
+    it "imports the correct number of entries" do
+     book.import_from_csv("entries_2.csv")
+     book_size = book.entries.size
+     expect(book_size).to eq(3)
+    end
+    it "imports the 1st entry" do
+     book.import_from_csv("entries_2.csv")
+     entry_one = book.entries[0]
+     check_entry(entry_one, "Frank", "555-555-4854", "frank@blocmail.com")
+    end
     
+    it "imports the 2nd entry" do
+       book.import_from_csv("entries_2.csv")
+       entry_two = book.entries[1]
+       check_entry(entry_two, "Jane", "555-555-5415", "jane@blocmail.com")
+     end
+ 
+     it "imports the 3rd entry" do
+       book.import_from_csv("entries_2.csv")
+       entry_three = book.entries[2]
+       check_entry(entry_three, "John", "555-555-3660", "john@blocmail.com")
+     end
+   end
    
    
    describe "#remove_entry" do
