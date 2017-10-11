24978c901925df76c9e7267097720bbd4aa91cb7
twr349 <twr349@gmail.com>
2017-10-11 15:56:41 +0000

    Checkpoint 8 completed

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -98,9 +98,61 @@ require_relative '../models/address_book'
  end
  
  def search_entries
+  print "Search by name: "
+  name = gets.chomp
+  match = address_book.binary_search(name)
+  system "clear"
+  
+  if match
+   puts match.to_s
+   search_submenu(match)
+  else
+   puts "No match found for #{name}"
+  end
  end
  
  def read_csv
+  
+   print "Enter CSV file to import: "
+   file_name = gets.chomp
+   
+   if file_name.empty?
+    system "clear"
+    puts "No CSV file read"
+    main_menu
+   end
+   
+   begin
+       entry_count = address_book.import_from_csv(file_name).count
+       system "clear"
+       puts "#{entry_count} new entries added from #{file_name}"
+     rescue
+       puts "#{file_name} is not a valid CSV file, please enter the name of a valid CSV file"
+       read_csv
+   end
+   
+ end
+ 
+ def delete_entry(entry)
+   address_book.entries.delete(entry)
+   puts "#{entry.name} has been deleted"
+ end
+ 
+ def edit_entry(entry)
+  print "Update name: "
+  name = gets.chomp
+  print "Update phone number: "
+  phone_number = gets.chomp
+  print "Update email: "
+  email = gets.chomp
+  
+  entry.name = name if !name.empty?
+  entry.phone_number = phone_number if !phone_number.empty?
+  entry.email = email if !email.empty?
+  system "clear"
+  
+  puts "Updated entry: "
+  puts entry
  end
  
  def entry_submenu(entry)
@@ -116,9 +168,10 @@ require_relative '../models/address_book'
     when "n"
      
     when "d"
-    
+     delete_entry(entry)
     when "e"
-    
+     edit_entry(entry)
+     entry_submenu(entry)
     when "m"
      system "clear"
      main_menu
@@ -130,6 +183,33 @@ require_relative '../models/address_book'
      
    end
   
+ end 
+ 
+ def search_submenu(entry)
+  puts "\nd - delete entry"
+  puts "e - edit this entry"
+  puts "m - return to main menu"
+  
+  selection = gets.chomp
+  
+  case selection
+    when "d"
+     system "clear"
+     delete_entry(entry)
+     main_menu
+    when "e"
+     edit_entry(entry)
+     system "clear"
+     main_menu
+    when "m"
+     system "clear"
+     main_menu
+    else
+     system "clear"
+     puts "#{slection} is not valid input"
+     puts entry.to_s
+     search_submenu(entry)
+  end
  end 
  
 
